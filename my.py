75ce44fed821bfe6237e51616272b5415d3a108f
Khaled Ahmed <www.khaledah826@gmail.com>
2025-05-22 01:38:55 +0300

    pfpf

--- my.py
+++ my.py
@@ -5,7 +5,8 @@ app = Flask(__name__)
 app.secret_key = 'your_secret_key_here'
 
 # Database configuration
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:@localhost/your_database_name'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://liberary:SHsharaf1234567@liberary.mysql.pythonanywhere-services.com/your_database_name'
+
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
