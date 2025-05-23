42e5df317b2b6cf3e1c715b6bc7436479439009f
Khaled Ahmed <www.khaledah826@gmail.com>
2025-05-23 19:57:12 +0300

    ooooooo

--- app.py
+++ app.py
@@ -1,19 +1,22 @@
 
-import pg8000
-from flask import Flask, render_template, request, redirect, flash, session, url_for
+from flask import Flask, render_template, request, redirect, url_for, flash, jsonify
 from flask_sqlalchemy import SQLAlchemy
-
+from flask_migrate import Migrate
+import os
+from datetime import datetime
 
 app = Flask(__name__)
 app.secret_key = 'your_secret_key_here'
   
 
 # Database configuration
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://librarysdb_user:I1FCS3WWae7KSZ8rEvyxITw7gCW6Otey@dpg-d0o9v8mmcj7s73e9usvg-a:5432/librarysdb'
-
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+pg8000://librarysdb_user:I1FCS3WWae7KSZ8rEvyxITw7gCW6Otey@dpg-d0o9v8mmcj7s73e9usvg-a:5432/librarysdb'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
+
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
 class SupportAdmin(db.Model):
     __tablename__ = 'super_admins'  # اسم الجدول في قاعدة البيانات
     id = db.Column(db.Integer, primary_key=True)
